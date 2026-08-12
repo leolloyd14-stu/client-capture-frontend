29fab4e9afa8a1ab1f24e7a489b9c511cef03400
leolloyd14-stu <adjeimensahlloyd@gmail.com>
2026-08-12 15:56:32 +0000

    Allow GitHub Pages frontend

--- backend/main.py
+++ backend/main.py
@@ -90,6 +90,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://leolloyd14-stu.github.io/client-capture-frontend/", 
     ],
     allow_credentials=True,
     allow_methods=[
